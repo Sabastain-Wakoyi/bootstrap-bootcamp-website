322635040afbe33219ff73c7a5f7a07f86efc607
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-12-29 10:05:31 -0600

    registered users

--- jascript.js
+++ jascript.js
@@ -1,4 +1,8 @@
+
 function SubmitDetails() {
+
+    var Num = 1
+
     var Name = document.getElementById('Name').value
     var number = document.getElementById('number').value
     var email = document.getElementById('email').value
@@ -6,6 +10,23 @@ function SubmitDetails() {
     var DOJ = document.getElementById('DOJ').value
     var expertise = document.getElementById('expertise').value
 
+    var tbody = document.getElementById('tbody')
+
     alert('Name: '+Name+'\n\nPhone Number: '+number+'\n\nEmail: '+email+'\n\nDate of Birth: '+DOB+'\n\nDate of Joining: '+DOJ+'\n\nExpertise: '+expertise)
+
+    tbody.innerHTML +=
+        <tr>
+            <th scope="row">${Num}</th>
+            <td>$Name</td>
+            <td>$number</td>
+            <td>$email</td>
+            <td>$DOB</td>
+            <td>$DOJ</td>
+            <td>$Expertise</td>
+
+        </tr>
+
+
+    Num++
 
 }
